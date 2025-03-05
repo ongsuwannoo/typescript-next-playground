63e6948aa68fe22f6d604e17eac63493f281d270
Jakkrapat Suwanno (Ong) <ongsuwannoo@gmail.com>
2025-03-05 12:21:54 +0700

    update(test): 1.1

--- src/components/templates/project-detail/index.tsx
+++ src/components/templates/project-detail/index.tsx
@@ -106,7 +106,7 @@ export default function ProjectDetail({ projectId }: { projectId: string }) {
             setVisible(true);
           }}
         >
-          Project Detail {projectId} Feature 1
+          Project Detail {projectId} Feature 1-test
         </Button>
         <Table<IDataPerson> columns={columns} data={data} />
       </Space>
